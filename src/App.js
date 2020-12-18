5ce8d81dda2d4da7494769aca3d7a1d833bef720
Michelle Andersson <michelleandersson@live.com>
2020-12-18 11:50:26 +0100

    add basic useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"; 
+import React, {useState, useEffect} from "react"; 
 import './App.css';
 // import frog from "./nedladdning.jpg"; 
 
@@ -74,11 +74,22 @@ function App() {
 // )
 
 const [emotion, setEmotion] = useState("happy"); 
+const [secondary, setSecondary] = useState("tired"); 
+
+useEffect (() => {
+  console.log(`It's ${emotion} around here...`);
+}, [emotion]); 
+
+useEffect(() => {
+  console.log(`It's ${secondary} around here...`);
+},[secondary])
 return (
   <>
-  <h1>Current mood is: {emotion}</h1>
-  <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-  <button onClick={() => setEmotion("enthuziastic!")}>Enthuziastic</button>
+  <h1>Current mood is: {emotion} and {secondary}</h1>
+  <button onClick={() => setEmotion("happy")}>Make Happy</button>
+  <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
+  <button onClick={() => setEmotion("frustrated...")}>Frustrate</button>
+  <button onClick={() => setEmotion("enthusiastic!")}>Enthuse</button>
   </>
 )
 
