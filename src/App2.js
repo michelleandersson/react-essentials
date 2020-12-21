69c0a27019dc3745a07243852c2cdaf725641c6f
Michelle Andersson <michelleandersson@live.com>
2020-12-21 16:18:11 +0100

    add loading, data, and error state to api call

--- src/App2.js
+++ src/App2.js
@@ -6,22 +6,29 @@ import './App.css';
 
 function App2({login}) {
     const [data, setData] = useState(null); 
+    const [loading, setLoading] = useState(false); 
+    const [error, setError] = useState(null); 
+
     useEffect(() => {
+        if(!login) return; 
+        setLoading(true); 
       fetch(`https://api.github.com/users/${login}`)
       .then(Response => Response.json())
-      .then(setData); 
-    }, []);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);  
+    }, [login]);
+
+    if(loading) return <h1>Loading...</h1>; 
+    if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>; 
+    if(!data) return null; 
   
-    if(data) {
-      return (<div>
-          <h1>{data.name}</h1>
-          <p>{data.location}</p>
-          <img alt={data.login} src={data.avatar_url} />
-      </div>)
-    } else {
-      return <p>No user Available</p>
-    } 
-  
+    return (<div>
+        <h1>{data.name}</h1>
+        <p>{data.location}</p>
+        <img height={200} alt={data.login} src={data.avatar_url} />
+    </div>
+    ); 
   }
 
   export default App2; 
