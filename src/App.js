a7a09a37431138dd789992da5865f4d7bdd8e38d
Michelle Andersson <michelleandersson@live.com>
2020-12-21 14:51:24 +0100

    re-add all parts to one return

--- src/App.js
+++ src/App.js
@@ -1,116 +1,111 @@
 import React, {useState, useEffect, useReducer} from "react"; 
 import './App.css';
-// import frog from "./nedladdning.jpg"; 
+import frog from "./nedladdning.jpg"; 
 
-// function Header (props) {
-//   return (
-//     <header>
-//       <h1>{props.name}'s Kitchen</h1>
-//     </header>
-//   )
-// }
-
-// function Main (props) {
-//   return (
-//     <section>
-//       <p>
-//         We serve the most {props.adjective} food around!
-//       </p>
-//       <img src={frog} height={200} alt="Frog singing" />
-//       <ul style={{textAlign: "left"}}>
-//         {props.dishes.map((dish) => (
-//         <li key={dish.id}>{dish.title}</li>
-//         ))}
-//       </ul>
-//     </section>
-//   )
-// }
-
-// function Footer (props) {
-//   return (
-//     <footer>
-//       <p>Copyright: {props.year}</p>
-//     </footer>
-//   )
-// }
-
-// const dishes = [
-//   "Mac & Cheese", 
-//   "Salmon", 
-//   "Chicken Nuggets"
-// ]; 
-
-// const dishObjects = dishes.map((dish, i) => ({id:i, title:dish})); 
-
-
-function SecretComponent () {
-  return <h1>Secret Message</h1>
+function Header (props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  )
 }
 
-function RegularComponent () {
-  return <h1>Everyone can see this component.</h1>
+function Main (props) {
+  return (
+    <section>
+      <p>
+        We serve the most {props.adjective} food around!
+      </p>
+      <img src={frog} height={200} alt="Frog singing" />
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+        <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  )
 }
+
+function Footer (props) {
+  return (
+    <footer>
+      <p>Copyright: {props.year}</p>
+    </footer>
+  )
+}
+
+const dishes = [
+  "Mac & Cheese", 
+  "Salmon", 
+  "Chicken Nuggets"
+]; 
+
+const dishObjects = dishes.map((dish, i) => ({id:i, title:dish})); 
+
+
+// function SecretComponent () {
+//   return <h1>Secret Message</h1>
+// }
+
+// function RegularComponent () {
+//   return <h1>Everyone can see this component.</h1>
+// }
 
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     {/* <Header name="Mattias" />
-  //     <Main adjective="amazing" dishes={dishObjects} />
-  //     <Footer year={new Date().getFullYear()} /> */}
-  //   </div>
-  // );
-// if (props.authorized){
-//   return <SecretComponent />
-// } else {
-//   return <RegularComponent />
 
-// }
+  const [emotion, setEmotion] = useState("happy"); 
+  const [secondary, setSecondary] = useState("tired"); 
 
-// return (
-//   <>
-//   {props.authorized ? <SecretComponent /> : <RegularComponent /> }
-//   </>
-// )
+  useEffect (() => {
+    console.log(`It's ${emotion} around here...`);
+  }, [emotion]); 
 
-// const [emotion, setEmotion] = useState("happy"); 
-// const [secondary, setSecondary] = useState("tired"); 
+  useEffect(() => {
+    console.log(`It's ${secondary} around here...`);
+  },[secondary])
 
-// useEffect (() => {
-//   console.log(`It's ${emotion} around here...`);
-// }, [emotion]); 
+  const [checked, toggle] = useReducer(
+    (checked) => !checked, 
+    false
+    );
 
-// useEffect(() => {
-//   console.log(`It's ${secondary} around here...`);
-// },[secondary])
-// return (
-//   <>
-//   <h1>Current mood is: {emotion} and {secondary}</h1>
-//   <button onClick={() => setEmotion("happy")}>Make Happy</button>
-//   <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
-//   <button onClick={() => setEmotion("frustrated...")}>Frustrate</button>
-//   <button onClick={() => setEmotion("enthusiastic!")}>Enthuse</button>
-//   </>
-// )
+  return (
+    <>
+      {/* Kitchen Part */}
+      <div className="App">
+        <Header name="Mattias" />
+        <Main adjective="amazing" dishes={dishObjects} />
+        <Footer year={new Date().getFullYear()} /> 
+      </div>
+      {/* {props.authorized ? <SecretComponent /> : <RegularComponent /> }   */}
+      
 
+      {/* Mood Part   */}
+      <h1>Current mood is: {emotion} and {secondary}</h1>
+      <button onClick={() => setEmotion("happy")}>Make Happy</button>
+      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
+      <button onClick={() => setEmotion("frustrated...")}>Frustrate</button>
+      <button onClick={() => setEmotion("enthusiastic!")}>Enthuse</button>  
+      
+      {/* Checkbox Part */}
+      <input 
+        type="checkbox" 
+        value= {checked} 
+        onChange={toggle} 
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
+    </>  
+    );   
+             
+    // if (props.authorized){
+    //   return <SecretComponent />
+    // } else {
+    //   return <RegularComponent />
+    
+    // };
 
-const [checked, toggle] = useReducer(
-  (checked) => !checked, 
-  false
-  ); 
+}; 
 
-
-return (
-<>
-  <input 
-    type="checkbox" 
-    value= {checked} 
-    onChange={toggle} 
-  />
-  <p>{checked ? "checked" : "not checked"}</p>
-</>  
-)
-
-}
 
 export default App;
